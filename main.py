c5b29f6ae794625481f91b532ef0702f13c453b7
meowthpunk <decaydesigner@gmail.com>
2022-12-08 13:37:51 +1000

    Commited

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+
 from app import socket
 from app import app
+from app import db
 import os
 
 if __name__ == '__main__':
-    socket.run(app, debug=True, port=os.getenv("PORT", default=5000))
+    with app.app_context():
+        db.create_all()
+        socket.run(app, debug=True, port=os.getenv("PORT", default=5000))
